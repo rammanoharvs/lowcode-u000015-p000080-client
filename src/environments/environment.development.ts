73fa30cb2cdf5dd00a774258e3abd5ca8aa8a95b
rammanoharvs <rammanohar.nair@gmail.com>
2024-06-06 05:38:23 +0000

    Code updated for the deployment id:

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  cookieExpiry: 60, ///in minutes
+  apiUrl: " https://1f45-103-152-114-6.ngrok-free.app/api/api/",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  cookieExpiry: 60, ///in minutes
+  apiUrl: " https://1f45-103-152-114-6.ngrok-free.app/api/api/",
+};
+
